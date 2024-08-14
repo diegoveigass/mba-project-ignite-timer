8b4a002a492be6783006a40b361bb28a2e90e3b6
Diego Veiga <diego.veiga@xmobots.com.br>
2024-08-14 19:15:39 -0300

    initial commit

---
+++ src/components/header/index.tsx
@@ -0,0 +1,22 @@
+import { Scroll, Timer } from 'phosphor-react';
+
+import { HeaderContainer, NavMenu } from './styles';
+import logoImg from '../../assets/logo.svg';
+import { NavLink } from 'react-router-dom';
+
+export function Header() {
+  return (
+    <HeaderContainer>
+      <img src={logoImg} alt="" />
+
+      <NavMenu>
+        <NavLink to="/" title="Timer">
+          <Timer size={24} />
+        </NavLink>
+        <NavLink to="/history" title="Histórico">
+          <Scroll size={24} />
+        </NavLink>
+      </NavMenu>
+    </HeaderContainer>
+  );
+}
